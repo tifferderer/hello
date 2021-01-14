1a71aa0c51f64a503b89202bf143d2b683906d98
Tiffany Ferderer <tferderer@mail.greenriver.edu>
2021-01-14 15:33:18 -0500

    Added home page view

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+
+//This is my CONTROLLER
+
+
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -10,5 +14,10 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 $f3->set('DEBUG', 3);
 
+//define a default route(home page)
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
 //run fat free
 $f3->run();
